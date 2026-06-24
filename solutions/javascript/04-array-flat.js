c99ce242fb6e66a3147251b97bf2cddeb69df412
Sarraf <2323885@cognizant.com>
2026-06-24 18:13:20 +0530

    Fushing flatten array solutions

--- solutions/javascript/04-array-flat.js
+++ solutions/javascript/04-array-flat.js
@@ -4,7 +4,18 @@
  */
 
 function flatten(arr, depth = 1) {
-  // YOUR CODE HERE
+  if (!Array.isArray(arr)) {
+    return [arr];
+  }
+
+  return arr.reduce((acc, item) => {
+    if (Array.isArray(item) && depth > 0) {
+      acc.push(...flatten(item, depth - 1));
+    } else {
+      acc.push(item);
+    }
+    return acc;
+  }, []);
 }
 
 module.exports = { flatten };
